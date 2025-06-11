8904f0dae935717508fdd81f005c6752501e31da
AndersonHaah <Andersonhaah1@gmail.com>
2025-06-10 23:59:37 -0300

    inicio do js

---
+++ Inicio e fundamentos/Bloco de codigos/bloco.js
@@ -0,0 +1,13 @@
+/* Bloco de codigo é criado por abertura e fechamento de chaves { } dentro desses blocos podem ter uma ou mais sentenças de codigo, podem ter um ou mais blocos dentro um do outro */
+
+{
+    console.log("Passo #01")
+    console.log("Passo #02")
+    console.log("Passo #03")
+}
+
+
+{
+    console.log("Passo #04")
+    console.log("Passo #05")
+}
--- Inicio e fundamentos/Sentença_1/sentenca_1.js
+++ Inicio e fundamentos/Sentença_1/sentenca_1.js
@@ -1,3 +1,5 @@
 console.log("Bom dia!");
 console.log("Boa tarde!");
 console.log("Boa noite!");
+
+// Sentença de codigo é basicamente uma frase de codigo, pode ser ou não terminada na mesma linha 
