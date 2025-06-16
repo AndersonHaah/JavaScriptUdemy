6083fe87de04f556c0122ca626cd0b78d82bb2cd
AndersonHaah <Andersonhaah1@gmail.com>
2025-06-16 10:50:10 -0300

    esutudos

---
+++ Inicio e fundamentos/aulas/ex026/opAritmeticos.js
@@ -0,0 +1,10 @@
+const [a, b, c, d] = [3, 5, 1, 15]
+
+const soma = a + b + c + d
+const subtracao = d - b
+const multiplicacao = a * b
+const divisao = d / a 
+let modulo = a % 2
+if (modulo = 1) modulo = 'IMPAR' 
+
+console.log(soma, subtracao, multiplicacao, -divisao, modulo)
